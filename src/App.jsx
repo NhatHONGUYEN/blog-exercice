16f6612baf16049eda0f70dd4579b1bf3b396b60
NhatHONGUYEN <hnnhat67@gmail.com>
2024-07-16 17:57:18 +0200

    ✨ F01 : Add our blog layout and add firebase in our project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-slate-600">hellow blog</div>
+      <div className="card card-compact bg-base-100 w-96 shadow-xl">
+        <div className="text-center p-4 font-bold text-xl">Create A Post</div>
+        <div className="card-body">
+          <h2 className="card-title">Title:</h2>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <h2 className="card-title">Post:</h2>
+          <textarea
+            className="textarea textarea-bordered"
+            placeholder="Write your post here"
+          ></textarea>
+          <button className="btn btn-block">Submit Post</button>
+        </div>
+      </div>
     </>
   );
 }
